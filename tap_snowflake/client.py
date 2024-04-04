ee300d6d5749b9fa7b6b798e24015d38a5353541
cameronSMN <cameron.tarbotton@smn.no>
2024-04-04 11:17:32 +0000

    trying to fix

--- tap_snowflake/client.py
+++ tap_snowflake/client.py
@@ -89,6 +89,9 @@ class SnowflakeConnector(SQLConnector):
         # ARGS:   ({'database': 'BI', 'authenticate_with_dope': True},)
         config = args[0] or {}
 
+        print("args", args)
+        print("kwargs", kwargs)
+
         # DOPE - convert the database name to <project_name>__<workspace>__<database>
         if str(config.get('authenticate_with_dope')): 
             self.logger.info("Authenticating with DOPE.")
@@ -132,6 +135,15 @@ class SnowflakeConnector(SQLConnector):
         if self.config.get("authenticate_with_dope"):
             self.logger.info(f"Creating sqlalchemy engine using DOPE.")
 
+            # db = str(self.config.get('database'))
+            # project_name = get_project_name()
+            # workspace_name = get_workspace_name()
+
+            # if not workspace_name:
+            #     raise RuntimeError
+
+            # self.config["database"] = fully_qualified_database_name(project_name, workspace_name, db)
+
             engine = sqlalchemy.create_engine("snowflake://",  creator=lambda: get_session().snowflake_connection(database=self.config['database']), future=True)
 
             with get_session().snowflake_connection() as connection, connection.cursor() as cs:
@@ -164,6 +176,7 @@ class SnowflakeConnector(SQLConnector):
             if schema_name.lower() != "information_schema"
         ]
 
+        print("SCHEMA: ", schema_names)
         # DOPE - Use schema from config if provided otherwise search through all 
         # schemas except for dope (this throws an error)
         if self.config.get("authenticate_with_dope"):
@@ -184,6 +197,7 @@ class SnowflakeConnector(SQLConnector):
                     )
                     result.append(catalog_entry.to_dict())
 
+        exit(1)
         return result
 
     def get_table_profile(
